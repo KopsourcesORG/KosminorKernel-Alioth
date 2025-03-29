b27c9833e0f745473336f50e266a4759144f11ce
knkj55 <knkj55@proton.me>
2025-03-29 01:47:19 +0000

    Packer: update packer 2.0

--- android/Packer/customize.sh
+++ android/Packer/customize.sh
@@ -1,36 +1,49 @@
 #/usr/bin/sh
-# K-Nel AB Installer
+# K-Nel A-Only Installer
 # By KeJia
 
-ui_print '安装 K-Nel'
-ui_print '安装后在模块列表可能会出现名为 "K-Nel AB Installer"的模块，无实际作用，如果出现，请卸载'
-ui_print '在部分SU管理器上，脚本输出“安装成功”，而管理器输出错误，提示安装失败，这种情况无须理睬，内核已安装成功'
+ui_print " "
+ui_print "- Install K-Nel"
+ui_print " "
 
 if test -z '$(grep alioth /system/build.prop)';
 then 
-  echo '设备非 红米K40 / 小米11X / POCO F3，不能安装该内核'
+  ui_print "- Device is not ' Redmi K40 / Xiaomi 11X / POCO F3 ' , cannot install this kernel."
   exit 1
 fi
+
+export bootslot=$(getprop ro.boot.slot_suffix)
 
 cd $MODPATH/tools/
 chmod +x magiskboot
 
-ui_print '提取 Boot 镜像。。。'
-dd if=/dev/block/by-name/boot_a of=$MODPATH/boot.img
-ui_print '解包 Boot 镜像。。。'
+ui_print "- Extracting 'Boot' Image..."
+dd if=/dev/block/by-name/boot$bootslot of=$MODPATH/boot.img
+ui_print "- Unpacking 'Boot' Image..."
 ./magiskboot unpack $MODPATH/boot.img
-ui_print '替换内核。。。'
-cp $MODPATH/Image ./kernel
-cp $MODPATH/dtb ./kernel_dtb
-ui_print '打包 Boot 镜像。。。'
+ui_print "- Spliting The Kernel Package And Uploading The Kernel..."
+./magiskboot split $MODPATH/Image.gz-dtb
+ui_print "- Repacking 'Boot' Image..."
 ./magiskboot repack $MODPATH/boot.img
-ui_print '刷入 Boot 镜像。。。'
-dd if=new-boot.img of=/dev/block/by-name/boot_a
-dd if=new-boot.img of=/dev/block/by-name/boot_b
-ui_print '刷入 Dtbo 镜像。。。'
-dd if=$MODPATH/dtbo.img of=/dev/block/by-name/dtbo_a
-dd if=$MODPATH/dtbo.img of=/dev/block/by-name/dtbo_b
-ui_print '安装完成'
+ui_print "- Flashing 'Boot' Image..."
+dd if=new-boot.img of=/dev/block/by-name/boot$bootslot
+if [ -e $MODPATH/dtbo.img ];
+then 
+    ui_print "- Flashing 'Dtbo' Image..."
+    dd if=$MODPATH/dtbo.img of=/dev/block/by-name/dtbo$bootslot
+fi
+ui_print "- Install Successful!"
 
-ui_print '清理中。。。'
-rm -rf $MODPATH
+ui_print " "
+
+ui_print "- Cleaning GPU cache..."
+find /data/user_de/*/*/*cache/* -iname "*shader*" -exec rm -rf {} +
+find /data/data/* -iname "*shader*" -exec rm -rf {} +
+find /data/data/* -iname "*graphitecache*" -exec rm -rf {} +
+find /data/data/* -iname "*gpucache*" -exec rm -rf {} +
+find /data_mirror/data*/*/*/*/* -iname "*shader*" -exec rm -rf {} +
+find /data_mirror/data*/*/*/*/* -iname "*graphitecache*" -exec rm -rf {} +
+find /data_mirror/data*/*/*/*/* -iname "*gpucache*" -exec rm -rf {} +
+ui_print "- GPU Cache Cleaning Completed."
+
+ui_print " "
